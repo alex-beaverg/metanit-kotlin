f8e0b0b17c63ab0aa89336dd7e6c9a04cfdf3da1
Alexey Bobrikov <a.bobrikov@kpedev.by>
2025-10-13 09:52:20 +0300

    next part of chapter 5 is updated

--- src/main/kotlin/chapter05/01_GenericTypes.kt
+++ src/main/kotlin/chapter05/01_GenericTypes.kt
@@ -15,7 +15,11 @@ fun genericTypes() {
     for (item in getBiggest(arrayOf(1, 5, 7, 90), arrayOf(5, 3))) {
         println("Элемент наибольшего массива: $item")
     }
-    // Ограничения обощений:
+    // Ограничения обобщений:
+    println("Большее из двух значений 5 и 10: ${compareObjects(5, 10)}")
+    val discountPrice = DiscountPrice(0.85)
+    println("Цена 100 руб с учетом скидки 0.85: ${calculatePrice(100.0, discountPrice)}")
+    // Несколько ограничений:
 
 }
 
@@ -42,7 +46,26 @@ class Book<K, V>(val pages: K, val price: V) {
 }
 
 /** Функция с обобщениями */
-fun <T> getBiggest(args1: Array<T>, args2: Array<T>): Array<T>{
+fun <T> getBiggest(args1: Array<T>, args2: Array<T>): Array<T> {
     if(args1.size > args2.size) return args1
     return  args2
 }
+
+/** Функция с обобщениями с ограничением */
+fun <T: Comparable<T>> compareObjects(arg1: T, arg2: T): T {
+    if(arg1 > arg2) return arg1
+    return  arg2
+}
+
+/** Интерфейс с одним свойством */
+interface Discount {
+    val discountValue: Double
+}
+
+/** Функция с обобщением с ограничением */
+fun<T: Discount> calculatePrice(price: Double, discount: T) : Double {
+    return price * discount.discountValue
+}
+
+/** Класс, реализующий интерфейс с ограниченным обобщением */
+class DiscountPrice(override val discountValue: Double) : Discount
