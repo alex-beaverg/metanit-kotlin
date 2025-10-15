2b5c5c5ade6b196cd7d74ac22b55ba9930573a16
Alexey Bobrikov <a.bobrikov@kpedev.by>
2025-10-15 13:46:15 +0300

    next part of chapter 6 is uploaded

--- src/main/kotlin/chapter06/03_ConvertTypes.kt
+++ src/main/kotlin/chapter06/03_ConvertTypes.kt
@@ -1,6 +1,41 @@
 package chapter06
 
 fun convertTypes() {
-    // Преобразование типов:
+    // ПРЕОБРАЗОВАНИЕ ТИПОВ
+    // Базовая реализация:
+    var str = "55"
+    val num: Int = str.toInt()
+    println("Это преобразованное в Int число из String: $num")
+    // Такие преобразования лучше делать через исключения:
+    str = "Число"
+    try {
+        println("Это не выведется на печать: ${str.toInt()}")
+    } catch(e: Exception){
+        println("Исключение при преобразовании типов: ${e.message}")
+    }
+    // Умное преобразование Smart Cast с оператором "is" и обратным "!is":
+    val dog = Pet("Тузик")
+    val cat = Cat("Мурзик", "Черный")
+    println("Проверим, является ли ${dog.name} котом (нет): ${isKitty(dog)}")
+    println("Проверим, является ли ${dog.name} не котом (да): ${isNotKitty(dog)}")
+    println("Проверим, является ли ${cat.color} ${cat.name} котом (да): ${isKitty(cat)}")
+    println("Проверим, является ли ${cat.color} ${cat.name} не котом (нет): ${isNotKitty(cat)}")
+    // К изменяемым типам var (в свойствах) оператор is не применяется:
 
+}
+
+/** Класс для наследования */
+open class Pet(val name: String)
+
+/** Класс наследник */
+class Cat(name: String, val color: String): Pet(name)
+
+/** Функция проверки на котика */
+fun isKitty(pet: Pet) : Boolean {
+    return pet is Cat
+}
+
+/** Функция проверки на не котика */
+fun isNotKitty(pet: Pet) : Boolean {
+    return pet !is Cat
 }
