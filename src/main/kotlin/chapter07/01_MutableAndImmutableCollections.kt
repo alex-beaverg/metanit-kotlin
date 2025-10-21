0dffd3b9e54c84da635e87785736917aecdcd8f4
Alexey Bobrikov <a.bobrikov@kpedev.by>
2025-10-21 12:08:08 +0300

    next part of chapter 7 is uploaded

--- src/main/kotlin/chapter07/01_MutableAndImmutableCollections.kt
+++ src/main/kotlin/chapter07/01_MutableAndImmutableCollections.kt
@@ -1,5 +1,6 @@
 package chapter07
 
+import jdk.internal.org.jline.keymap.KeyMap.key
 import java.util.Date
 
 fun mutableAndImmutableCollections() {
@@ -20,6 +21,7 @@ fun mutableAndImmutableCollections() {
     list.remove(3)
     list.removeAt(0)
     list.forEach { element -> println("\t- $element") }
+    println("Или так можно вывести: $list")
     println("Второй элемент списка: ${list[1]}") // Равносильно list.get(1)
     println("Несуществующий элемент списка без генерации исключения: ${list.getOrNull(10)}")
     println("Последний элемент списка вместо несуществующего: ${list.getOrElse(10) { list[list.size - 1] }}")
@@ -33,14 +35,24 @@ fun mutableAndImmutableCollections() {
     val newSet = set.filter { it.toInt() < 3 }
     println("Убрали элемент по фильтру:")
     newSet.forEach { element -> println("\t- $element") }
+    println("Или так можно вывести: $set")
     val nextNewSet = newSet.plus("500")
     println("Добавили элемент плюсом:")
     nextNewSet.forEach { element -> println("\t- $element") }
     val unionSet = set.union(nextNewSet)
     println("Объединили два множества:")
     unionSet.forEach { element -> println("\t- $element") }
-
+    println("Пересекающиеся элементы множеств: ${newSet.intersect(set)}")
+    println("Пересекающиеся элементы множеств (другая конструкция): ${newSet intersect set}")
+    println("Вычитание множеств: ${unionSet.subtract(set)}")
+    println("Вычитание множеств (другая конструкция): ${unionSet subtract(set)}")
+    val newMutableSet = unionSet.toMutableSet()
+    newMutableSet.addAll(setOf("350", "820", "54"))
+    println("Перевели множество в изменяемое и добавили элементы: $newMutableSet")
     // Map:
+    val map = mutableMapOf(Pair(1, "One"), 2 to "Two", 3 to "Three")
+    println("Карта: $map")
+    map.forEach { (key, value) -> println("\t$key: $value") }
 
     // Sequence:
     val sequence = sequenceOf(100, 200, 300)
