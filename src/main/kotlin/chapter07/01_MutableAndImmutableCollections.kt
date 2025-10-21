8ceb459f5a81e9a62e9921be153552b328dda00f
Alexey Bobrikov <a.bobrikov@kpedev.by>
2025-10-21 10:22:13 +0300

    first part of chapter 7 is uploaded

--- src/main/kotlin/chapter07/01_MutableAndImmutableCollections.kt
+++ src/main/kotlin/chapter07/01_MutableAndImmutableCollections.kt
@@ -1,6 +1,16 @@
 package chapter07
 
+import java.util.Date
+
 fun mutableAndImmutableCollections() {
-    // Изменяемые и неизменяемые коллекции:
+    // ИЗМЕНЯЕМЫЕ И НЕИЗМЕНЯЕМЫЕ КОЛЛЕКЦИИ:
+    // Неизменяемые коллекции:
+    val imList = if (Date().time % 2 == 0L) listOf() else listOf(1, 2, 3)
+    println("Пустой ли лист: ${imList.isEmpty()}")
+    val imSet = setOf("1", "2", "3")
+    println("Размер множества: ${imSet.size}")
+    val imMap = mapOf(Pair(1, "One"), Pair(2, "Two"))
+    println("Содержит ли карта значение по ключу 2: ${imMap.contains(2)}")
+    // Изменяемые коллекции:
 
 }
